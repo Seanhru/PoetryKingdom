ebf2e53c5c2524eaa844b782402a8ce671ec97df
Arnab Ghosh <arnabcare21@gmail.com>
2024-05-29 19:02:36 -0700

    Add prettier to project

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  trailingComma: "all",
+  tabWidth: 4, 
+  useTabs: true, 
+  semi: true, 
+  singleQuote: false,
 };
 
 export default config;
